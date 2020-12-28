a20cabbd435ab61263fbb53a441a08896aa5888a
Nina Denisova <ninakuz93@gmail.com>
2020-12-28 23:34:53 +0500

    Add create post mailer test

---
+++ spec/mailers/create_post_mailer_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe CreatePostMailer, type: :mailer do
+  let(:post) { create :post }
+  let(:mail) { CreatePostMailer.create_post_mail(post) }
+  it "renders the headers" do
+    expect(mail.subject).to eq("Create post mail")
+    expect(mail.to).to eq([post.user.email])
+    expect(mail.from).to eq(["from@example.com"])
+  end
+  it "renders the body" do
+    expect(mail.body.encoded).to match(post.user.email)
+    expect(mail.body.encoded).to match(post.id.to_s)
+  end
+end
